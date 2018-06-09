89a0d84942388540b4e4507f9070bf1ee392ff2f
Ashish <ashusnp@gmail.com>
2018-06-09 09:31:49 +0530

    add test for get /todos

--- test/server.test.js
+++ test/server.test.js
@@ -3,9 +3,18 @@ const request=require('supertest');
 
 var {app}=require('../server/server');
 var {ToDo}=require('../model/ToDo');
+const todos1=[{
+  text:'First text'
+},{
+  text:'Second Text'
+}];
 beforeEach((done)=>{
-  ToDo.remove({}).then(()=>done());
-});// for clearing db each time
+    ToDo.remove({}).then(()=>{
+      return ToDo.insertMany(todos1);
+    }).then(()=>{
+      done();
+    });
+});      // for clearing db each time
 describe('Post ToDOs',()=>{
   var text="First test";
 it('should be post',(done)=>{
@@ -20,7 +29,7 @@ it('should be post',(done)=>{
     if(err){
       return done(err);
     }
-    ToDo.find().then((todos)=>{ // to check data in mongodb
+    ToDo.find({text}).then((todos)=>{ // to check data in mongodb
       expect(todos.length).toBe(1);
       expect(todos[0].text).toBe(text);
       done();
@@ -28,24 +37,38 @@ it('should be post',(done)=>{
     }).catch((err)=>done(err));
   });
 });
-var text1="";
+
 //*******************************************************************************//
 it("should not send invalid body",(done)=>{
   request(app)
   .post('/todos')
-  .send({text:text1})
+  .send({})
   .expect(400)
   .end((err,res)=>{
     if(err){
       return done(err);
     }
     ToDo.find().then((todos)=>{
-      expect(todos.length).toBe(0);
+      expect(todos.length).toBe(2);
       done();
     }).catch(()=>done());
   })
 });
 
-
+//************************************************************************//
+it('should get todo',(done)=>{
+  request(app)
+  .get('/todos')
+  .expect(200)
+  .end((err,res)=>{
+    if(err){
+      return done(err);
+    }
+    ToDo.find().then((todos)=>{
+      expect(todos.length).toBe(2);
+      done();
+    }).catch(()=>done());
+  })
+})
 
 });
