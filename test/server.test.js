5f0cb8bbd6018b34da2c06d8a4926944c4a210eb
Ashish <ashusnp@gmail.com>
2018-06-09 14:24:22 +0530

    get test cases todo by id

--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,13 @@
 const expect=require('expect');
 const request=require('supertest');
-
+const {ObjectID}=require('mongodb');
 var {app}=require('../server/server');
 var {ToDo}=require('../model/ToDo');
 const todos1=[{
+  _id:new ObjectID(),
   text:'First text'
 },{
+  _id:new ObjectID(),
   text:'Second Text'
 }];
 beforeEach((done)=>{
@@ -70,5 +72,38 @@ it('should get todo',(done)=>{
     }).catch(()=>done());
   })
 })
+
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+describe('find by id',()=>{
+  var id='5b1b8904d4a88e1ff8ee0a79';
+it("should find by id",(done)=>{
+request(app)
+.get(`/todo/${todos1[0]._id.toHexString()}`)
+.expect(200)
+.expect((res)=>{
+  expect(res.body.todo.text).toBe(todos1[0].text);
+})
+.end(done);
+});
+
+it('should return 404 if todo not found',(done)=>{
+  request(app)
+  .get(`/todo/${new ObjectID().toHexString()}`)
+  .expect(404)
+  // .expect((res)=>{
+  //   console.log(res.body);
+  //   expect(res.body).toBe('not found');
+  // })
+  .end(done);
+});
+
+it('should check id is valid or not',(done)=>{
+request(app)
+.get(`/todo/123`)
+.expect(404)
+.end(done);
+});
 
 });
