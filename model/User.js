c5b9ab0c8111a949c5a3966483c66e24fa5defdc
Ashish <ashusnp@gmail.com>
2018-06-16 16:56:35 +0530

    hash password

--- model/User.js
+++ model/User.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose');
 const validator=require('validator');
 const jwt=require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt=require('bcryptjs');
 var UserSchema=new mongoose.Schema({
   email:{
     type:String,
@@ -57,14 +58,33 @@ UserSchema.statics.findByToken=function(token){
   });
 
 }
+
+UserSchema.pre('save',function(next){
+  var user=this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err,hashPasswordValue)=>{
+        if(err){
+
+        }
+        user.password=hashPasswordValue;
+        next();
+      });
+    });
+  }
+  else{
+    next();
+  }
+});
 UserSchema.methods.generateAuthToken=function() {
   console.log('here');
   var user=this;
   var access='auth';
   var token=jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
   //user.tokens.push({access,token});
-user.tokens=user.tokens.concat([{access,token}]);
-console.log(user.tokens);
+  user.tokens=user.tokens.concat([{access,token}]);
+  console.log(user.tokens);
   return  user.save().then(()=>{
     return token;
   }).catch((e)=>{
--- playgroud/hashing.js
+++ playgroud/hashing.js
@@ -1,5 +1,19 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
+
+var password="abc123!";
+var hv='$2a$10$Sl.EU6B7G0VRXYmC5wQ0ROfFZr2/RLJOwzCjZlO3oitKYb8VPK2cG';
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hashValue)=>{
+    console.log("hashValue ",hashValue);
+    //hv=hashValue;
+  })
+});
+bcrypt.compare(password,hv,(err,result)=>{
+  console.log('result ',result);
+})
+////////////////////////////////////
 var msg="I love Dhanshree";
 var hash=SHA256(msg).toString();
 console.log(msg);
